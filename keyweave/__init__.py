83054611b30b7be0966cfe516851528e36e9bd9c
GregRos <life.gregr@gmail.com>
2025-10-16 20:34:57 +0300

    fix: export with __all__ for pyright validation

--- keyweave/__init__.py
+++ keyweave/__init__.py
@@ -15,3 +15,20 @@ from keyweave.hotkey import HotkeyEvent
 from keyweave.commanding import command, Command, CommandProducer
 from keyweave.key_types import Key
 from keyweave.interception import HotkeyInterceptionEvent
+
+__all__ = [
+    "Key",
+    "KeyInput",
+    "KeySet",
+    "KeysInput",
+    "KeyInputState",
+    "LayoutClass",
+    "Layout",
+    "scheduling",
+    "key",
+    "HotkeyEvent",
+    "command",
+    "Command",
+    "CommandProducer",
+    "HotkeyInterceptionEvent",
+]
